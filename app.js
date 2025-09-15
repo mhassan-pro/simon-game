b57868881ffff26ff84666323db92f6095c4eceb
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-15 19:23:50 +0500

    Fix level display and enhance button flash effect in Simon Game

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["red","yellow","green","purple"];
+
 let started = false;
 let level = 0;
 
@@ -10,11 +12,22 @@ document.addEventListener('keypress', function(){
     if(started == false){
         console.log("Game is started");
         started = true;
+
+        levelUp();
     }
 }); 
 
 function levelUp () {
     level++;
-    h2.inerText = "Level" + level; 
+    h2.inerText = `Level ${level}`;
+    
+    btnFlash();
+}
+
+function btnFlash(btn){
+    btnFlash.classList.add("flash");
+    setTimeout(function(){
+        btnFlash.classList.remove("flash")},1000
+    )
 
 }
