bd8b8ea32ddc7525aa594f5280aad49b335943bd
mhassan-pro <mhassan.pro0@gmail.com>
2025-09-15 19:16:12 +0500

    Initialize game logic and update button styles for Simon Game

---
+++ app.js
@@ -0,0 +1,20 @@
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener('keypress', function(){
+    if(started == false){
+        console.log("Game is started");
+        started = true;
+    }
+}); 
+
+function levelUp () {
+    level++;
+    h2.inerText = "Level" + level; 
+
+}
